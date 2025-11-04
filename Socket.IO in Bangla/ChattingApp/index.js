4a9a4b953f9189c26bd26dd6235238166a6b6a7f
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-04 13:37:42 +0600

    Startmaking chatting app

---
+++ Socket.IO in Bangla/ChattingApp/index.js
@@ -0,0 +1,23 @@
+//create express server
+const express = require("express");
+const app = express();
+const http = require("http");
+const expressServer = http.createServer(app);
+const PORT = 3000;
+const path = require("path");
+
+//Create socket server
+const { Server } = require("socket.io");
+const socketServer = new Server(expressServer);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+socketServer.on("connection", (socket) => {
+  socket.send("Hello Subrata");
+});
+
+expressServer.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});

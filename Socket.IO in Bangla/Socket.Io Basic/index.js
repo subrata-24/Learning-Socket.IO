c6920137ee67e903f3264078fa8dfb54237fe540
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-04 13:08:03 +0600

    Create namespace to separate communication channel so that client can establish connection with separate chanel as their need

--- Socket.IO in Bangla/Socket.Io Basic/index.js
+++ Socket.IO in Bangla/Socket.Io Basic/index.js
@@ -1,29 +1,47 @@
-//make http express server
+// Import required modules
 const express = require("express");
 const app = express();
 const http = require("http");
+
+// Create an HTTP server using the Express app
 const expressServer = http.createServer(app);
 const PORT = 3000;
 const path = require("path");
 
-//make socket.io server
+// Import and setup Socket.IO server
 const { Server } = require("socket.io");
-const io = new Server(expressServer);
+const io = new Server(expressServer); // This sets up the main Socket.IO server
 
+// Serve the HTML page on root route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-io.on("connection", (socket) => {
-  console.log("User is connected");
-  //If use io. then it send the message or change to all conneceted client with this server
-  //If use socket. it refers to only one client who is currently connected or whom data is change.Only he get the updated message/change.
-  io.sockets.emit(
-    "broadcastEvent",
-    "Broadcast this message to all users who are connected"
-  );
+/*
+  Create and configure custom namespaces:
+  - A namespace is a separate communication "channel" under the same Socket.IO server.
+  - Clients must explicitly connect to a namespace to exchange events/messages through it.
+*/
+
+// Create '/chat' namespace
+let chatSocket = io.of("/chat");
+
+// Handle connections to '/chat' namespace
+chatSocket.on("connection", (socket) => {
+  // Emit 'customEvent' to ALL clients connected to /chat namespace
+  chatSocket.emit("customEvent", "Lets start chatting");
 });
 
+// Create '/admin' namespace
+let adminSocket = io.of("/admin");
+
+// Handle connections to '/admin' namespace
+adminSocket.on("connection", (socket) => {
+  // Emit 'customEvent' to ALL clients connected to /admin namespace
+  adminSocket.emit("customEvent", "I am only for admin panel");
+});
+
+// Start the HTTP server
 expressServer.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
